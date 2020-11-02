48dd0d275ecc9f1ea1e93622f61438d79fabdb0b
serhiisad <serhiisad.kpi@gmail.com>
2020-11-02 10:23:41 +0200

    spa test app

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # react-test-app
-technical test task.
+
+Test SPA
--- testr.js
+++
@@ -1,6 +0,0 @@
-const fetch = require("node-fetch");
-
-fetch("news.json")
-  .then((resp) => resp.text())
-  .then((data) => console.log(data))
-  .catch((err) => console.error(err));
